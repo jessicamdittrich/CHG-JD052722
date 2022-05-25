cc2f2c9f0fd9a2d0262fa1d5d255d0612ac0cfb1
Jessica Dittrich <104876367+jessicamdittrich@users.noreply.github.com>
2022-05-25 16:42:34 -0700

    Work with tutor that made things worse and more confusing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,72 @@
-// TIME RELATED VARIABLES
+// TIMER VARIABLES
 var today = moment();
-var time = (today.format("h a"));
+var time = (today.format("h"));
+var timeDiv = $('.custom-time');
+var inputTextDiv = $('.custom-text');
+var saveBtn = $('.btn');
 
 // TELL THE DATE AND TIME
 $("#currentDay").text(today.format("dddd, MMMM Do YYYY, h:mm a"));
 
-var timeDiv = $('.custom-time');
-var inputTextDiv = $('.custom-text');
-
-console.log(timeDiv[0]);
-
+// function test(arg){
+//     console.log(arg)
+// }
+// FUNCTION TO MAKE COLOURS CHANGED BASED ON TIME OF DAY
 function tellTime(){
 
-    for (var i = 0; i < timeDiv[i].outerText; i++){
-        console.log(timeDiv[i]);   
-    }
+    for (var i = 0; i < timeDiv.length; i++){
+        if (time >= inputTextDiv[i].dataset.time){
+            console.log(timeDiv[i].getAttribute("data-time"))
+            inputTextDiv.addClass('bg-success');
+        } 
+        else if (time == inputTextDiv[i].getAttribute("data-time")) {
+            inputTextDiv[i].addClass('bg-danger');
+        }/*
+        else if (time <= inputTextDiv[i].getAttribute("data-time")){
+            inputTextDiv[i].addClass('bg-secondary');
+        }*/
 
-    if (time >= timeDiv[i].outerText){
-        inputTextDiv.addClass('bg-success');
-    } 
-    else if (time == timeDiv[i].outerText) {
-        inputTextDiv.addClass('bg-danger');
-    }
-    else if (time <= timeDiv[i].outerText){
-        inputTextDiv.addClass('bg-secondary');
     }
 
     console.log(time);
-    console.log(timeDiv[0].outerText);
+    console.log(timeDiv[0].outerText.length);
 
 };
 
+for (var i = 0; i < saveBtn.length; i++){
+    saveBtn.on("click", saveInputs);
+}
+
+// SAVING TEXT INPUTS TO LOCAL STORAGE
+function saveInputs(event) {
+    event.preventDefault();
+
+    for (var i = 0; i < inputTextDiv.size; i++){
+        inputTextDiv.val(localStorage.getItem('calendarTasks'));
+        localStorage.setItem('calendarTasks', inputTextDiv.val());
+        console.log(inputTextDiv[0]);
+    }
+
+    console.log(inputTextDiv[0]);
+
+};
+
+// RUN FUNCTION
 tellTime();
+
+
+// loads screen and sees date and time
+    // edit the dom to reflect current time
+        // .text to change html
+// write down daily tasks
+    // give user a place to write
+        // input fields
+// save tasks
+    // buttons to save
+        // write a save function
+// save to local storage for persistence
+    // button function saves
+        // localStorage.setItem
+// as hours change so do blocks
+    // watch hours and change dom
+        // if else
