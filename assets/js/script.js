e55f6d4b5cae361fc4e4a0d0b70c30410a12128e
Jessica Dittrich <104876367+jessicamdittrich@users.noreply.github.com>
2022-05-25 20:50:56 -0700

    More work in js - completed colour change (on my own)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // TIMER VARIABLES
 var today = moment();
-var time = (today.format("h"));
+var time = (today.format("H"));
 var timeDiv = $('.custom-time');
 var inputTextDiv = $('.custom-text');
 var saveBtn = $('.btn');
@@ -8,65 +8,67 @@ var saveBtn = $('.btn');
 // TELL THE DATE AND TIME
 $("#currentDay").text(today.format("dddd, MMMM Do YYYY, h:mm a"));
 
-// function test(arg){
-//     console.log(arg)
-// }
 // FUNCTION TO MAKE COLOURS CHANGED BASED ON TIME OF DAY
 function tellTime(){
 
-    for (var i = 0; i < timeDiv.length; i++){
-        if (time >= inputTextDiv[i].dataset.time){
-            console.log(timeDiv[i].getAttribute("data-time"))
-            inputTextDiv.addClass('bg-success');
+    $.each(inputTextDiv, function( index, value ) {
+        if (time > value.dataset.time) {
+            $(this).addClass('bg-secondary');
         } 
-        else if (time == inputTextDiv[i].getAttribute("data-time")) {
-            inputTextDiv[i].addClass('bg-danger');
-        }/*
-        else if (time <= inputTextDiv[i].getAttribute("data-time")){
-            inputTextDiv[i].addClass('bg-secondary');
-        }*/
-
-    }
-
-    console.log(time);
-    console.log(timeDiv[0].outerText.length);
+        else if (time == value.dataset.time) {
+            $(this).addClass('bg-danger');   
+        }
+        else if (time < value.dataset.time){
+            $(this).addClass('bg-success');
+        }
+        //console.log(value.dataset.time);
+        //console.log(this);
+    });
 
 };
-
-for (var i = 0; i < saveBtn.length; i++){
-    saveBtn.on("click", saveInputs);
-}
 
 // SAVING TEXT INPUTS TO LOCAL STORAGE
-function saveInputs(event) {
+var textNine = $('.text-9');
+var btnNine = $('.btn-9')
+
+btnNine.on('click', function(event) {
     event.preventDefault();
+    textNine.get((localStorage.getItem('calendarTasks')));
+    localStorage.setItem('calendarTasks', textNine.val());
 
-    for (var i = 0; i < inputTextDiv.size; i++){
-        inputTextDiv.val(localStorage.getItem('calendarTasks'));
-        localStorage.setItem('calendarTasks', inputTextDiv.val());
-        console.log(inputTextDiv[0]);
-    }
-
-    console.log(inputTextDiv[0]);
-
-};
+    console.log((localStorage.getItem('calendarTasks')));
+    console.log(textNine.val());
+})
 
 // RUN FUNCTION
 tellTime();
 
+/*$(saveBtn).click(function() {
+    inputTextDiv.val(localStorage.getItem('calendarTasks'));
+    $(inputTextDiv).each(function() {
+        console.log(saveBtn);
+        console.log(inputTextDiv);
+        localStorage.setItem('calendarTasks', inputTextDiv.val());
+    });
+});
 
-// loads screen and sees date and time
-    // edit the dom to reflect current time
-        // .text to change html
-// write down daily tasks
-    // give user a place to write
-        // input fields
-// save tasks
-    // buttons to save
-        // write a save function
-// save to local storage for persistence
-    // button function saves
-        // localStorage.setItem
-// as hours change so do blocks
-    // watch hours and change dom
-        // if else
+jQuery.each(saveBtn, function( i, val ) {
+    $(this).on('click', function() {
+        localStorage.setItem('calendarTasks', inputTextDiv.val());
+    })
+});
+
+jQuery.each(inputTextDiv, function( i, val ) {
+    $(this).on('click', function() {
+        inputTextDiv.val(localStorage.getItem('calendarTasks'));
+    })
+});
+
+    $.each(saveBtn, function () {
+        $(this).on("click", function(){
+            console.log(this);
+            localStorage.setItem('calendarTasks', inputTextDiv.val());
+        })
+    });
+        inputTextDiv.val(localStorage.getItem('calendarTasks'));
+        localStorage.setItem('calendarTasks', inputTextDiv.val());*/
