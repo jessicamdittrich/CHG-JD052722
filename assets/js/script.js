27905845ed5675b4cbd90d18ec1aac665b1e6a09
Jessica Dittrich <104876367+jessicamdittrich@users.noreply.github.com>
2022-05-26 08:26:16 -0700

    Updated js but trying to figure out local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,47 +28,23 @@ function tellTime(){
 };
 
 // SAVING TEXT INPUTS TO LOCAL STORAGE
-var textNine = $('.text-9');
-var btnNine = $('.btn-9')
+$(document).ready(function() {
 
-btnNine.on('click', function(event) {
-    event.preventDefault();
-    textNine.get((localStorage.getItem('calendarTasks')));
+    console.log("document is ready");
+
+    var textNine = $('.text-9');
+    var btnNine = $('.btn-9');
+
     localStorage.setItem('calendarTasks', textNine.val());
 
-    console.log((localStorage.getItem('calendarTasks')));
-    console.log(textNine.val());
-})
+    btnNine.on('click', function(event) {
+        event.preventDefault();
+        textNine.value((localStorage.getItem('calendarTasks')));
+        console.log((localStorage.getItem('calendarTasks')));
+        console.log(textNine.val());
+        });
+
+});
 
 // RUN FUNCTION
 tellTime();
-
-/*$(saveBtn).click(function() {
-    inputTextDiv.val(localStorage.getItem('calendarTasks'));
-    $(inputTextDiv).each(function() {
-        console.log(saveBtn);
-        console.log(inputTextDiv);
-        localStorage.setItem('calendarTasks', inputTextDiv.val());
-    });
-});
-
-jQuery.each(saveBtn, function( i, val ) {
-    $(this).on('click', function() {
-        localStorage.setItem('calendarTasks', inputTextDiv.val());
-    })
-});
-
-jQuery.each(inputTextDiv, function( i, val ) {
-    $(this).on('click', function() {
-        inputTextDiv.val(localStorage.getItem('calendarTasks'));
-    })
-});
-
-    $.each(saveBtn, function () {
-        $(this).on("click", function(){
-            console.log(this);
-            localStorage.setItem('calendarTasks', inputTextDiv.val());
-        })
-    });
-        inputTextDiv.val(localStorage.getItem('calendarTasks'));
-        localStorage.setItem('calendarTasks', inputTextDiv.val());*/
